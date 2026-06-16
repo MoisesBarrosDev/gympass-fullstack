8d7b7315fb04b9e00a9fae87ad24eede558ca34a
Moisés Barros <moisesbarrosdev@gmail.com>
2026-06-16 14:58:23 -0300

    docs: adiciona anotação sobre configuração e validação de variáveis de ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import { app } from "./app.js";
-
+import { env } from "./env/index.js";
 try {
    await app.listen({
     host: "0.0.0.0",
-    port: 3333,
+    port: env.PORT,
   });
   console.log('HTTP Server Running!')
 } catch (error) {
