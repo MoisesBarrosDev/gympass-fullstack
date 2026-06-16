23758f5025e9dc44a04823d7b5785679a948fe08
Moisés Barros <moisesbarrosdev@gmail.com>
2026-06-16 19:53:06 -0300

    feat: configura Prisma Client, adapter PostgreSQL e model User

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,22 @@
-import fastify from "fastify"
+import fastify from "fastify";
+import { PrismaClient } from "./generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { env } from "./env/index.js";
 
-export const app = fastify ();
+export const app = fastify();
+
+const adapter = new PrismaPg({
+  connectionString: env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({
+  adapter,
+});
+
+await prisma.user.create({
+  data: {
+    name: "Moisés Barros",
+    email: "moisesbarros@gmail.com",
+  },
+});
+
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import { app } from "./app.js";
 import { env } from "./env/index.js";
+
 try {
-   await app.listen({
+  await app.listen({
     host: "0.0.0.0",
     port: env.PORT,
   });
-  console.log('HTTP Server Running!')
+  console.log("HTTP Server Running!");
 } catch (error) {
-    console.error(`O servidor não está funcionando error : ${error}`)
+  console.error(`O servidor não está funcionando error : ${error}`);
 }
