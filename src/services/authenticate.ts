faa5c88b9124888245d74944f6c551c1bdd2013a
Moisés Barros <moisesbarrosdev@gmail.com>
2026-07-21 21:39:26 -0300

    refactor: remove comentários desnecessários do serviço de autenticação

--- src/services/authenticate.ts
+++ src/services/authenticate.ts
@@ -3,13 +3,11 @@ import type { UsersRepository } from "../repositories/users-repository.js";
 import { InvalidCredentialsError } from "./errors/invalid-credential-error.js";
 import type { User } from "../generated/prisma/client.js";
 
-// Oque que a pessoa tem que enviar pra fazer a autenticação
 interface AuthenticateUseCaseRequest {
   email: string;
   password: string;
 }
 
-// Oque que eu espero devolver daqui de dentro pra saber que o usuario realmente foi autenticado ou não
 interface AuthenticateUseCaseResponse {
   user: User;
 }
@@ -27,10 +25,6 @@ export class Authenticate {
       throw new InvalidCredentialsError();
     }
 
-    // Boolean => "is", "has", "does"
-
-    // Toda vez que fomos usar uma variavel que retorna um boolean a leitura da variavel
-    // tem que ser semantica.
     const doesPasswordMatches = await compare(password, user.password_hash);
 
     if (!doesPasswordMatches) {
