949cea222bd424a660e3b2be201b2c739f059898
Moisés Barros <moisesbarrosdev@gmail.com>
2026-08-03 20:04:02 -0300

    feat: adiciona histórico paginado de check-ins

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ GymPass style app.
 - [x] Deve ser possível se autenticar;
 - [x] Deve ser possível obter o perfil de um usuário logado;
 - [ ] Deve ser possível obter o número de check-ins realizados pelo usuário logado;
-- [ ] Deve ser possível o usuário obter seu histórico de check-ins;
+- [x] Deve ser possível o usuário obter seu histórico de check-ins;
 - [ ] Deve ser possível o usuário buscar academias próximas;
 - [ ] Deve ser possível o usuário buscar academias pelo nome;
 - [ ] Deve ser possível o usuário realizar check-in em uma academia;
@@ -16,7 +16,7 @@ GymPass style app.
 - [x] Deve ser possível cadastrar uma academia;
 - [x] Deve ser possível excluir uma academia cadastrada;
 - [x] Deve ser possível recuperar uma academia excluída;
-- [ ] Deve ser possível ler todas academias cadastradas;
+- [x] Deve ser possível ler todas academias cadastradas;
 - [ ] Deve ser possível atualizar informações de academias cadastradas;
 
 ## Rns (Regras de negócio)
@@ -34,7 +34,7 @@ GymPass style app.
 
 - [x] A senha do usuário precisa estar criptografada;
 - [x] Os dados da aplicação precisam estar persistidos em um banco PostgreSQL;
-- [ ] Todas listas de dados precisam estar paginadas com 20 itens por página;
+- [x] Todas listas de dados precisam estar paginadas com 20 itens por página;
 - [ ] O usuário deve ser identificado por um JWT (JSON Web Token);
 
 
---
+++ src/services/fetch-user-check-ins-history.spec.ts
@@ -0,0 +1,56 @@
+import { beforeEach, describe, expect, test } from "vitest";
+import { InMemoryCheckInsRepository } from "../repositories/in-memory/in-memory-checkins-repository.js";
+import { FetchUserCheckInsHistory } from "./fetch-user-check-ins-history.js";
+
+let inMemoryCheckInsRepository: InMemoryCheckInsRepository;
+let sut: FetchUserCheckInsHistory;
+
+describe("Fetch User Check-in History Use Case", () => {
+  beforeEach(async () => {
+    inMemoryCheckInsRepository = new InMemoryCheckInsRepository();
+    sut = new FetchUserCheckInsHistory(inMemoryCheckInsRepository);
+  });
+
+  test("should be able to fetch check-in history", async () => {
+    await inMemoryCheckInsRepository.create({
+      gym_id: "gym-01",
+      user_id: "user-01",
+    });
+
+    await inMemoryCheckInsRepository.create({
+      gym_id: "gym-02",
+      user_id: "user-01",
+    });
+
+    const { checkIns } = await sut.execute({
+      userId: "user-01",
+      page: 1,
+    });
+
+    expect(checkIns).toHaveLength(2);
+    expect(checkIns).toEqual([
+      expect.objectContaining({ gym_id: "gym-01" }),
+      expect.objectContaining({ gym_id: "gym-02" }),
+    ]);
+  });
+
+  test("should be able to fetch paginated check-in history", async () => {
+    for (let i = 1; i <= 22; i++) {
+      await inMemoryCheckInsRepository.create({
+        gym_id: `gym-${i}`,
+        user_id: "user-01",
+      });
+    }
+
+    const { checkIns } = await sut.execute({
+      userId: "user-01",
+      page: 2,
+    });
+
+    expect(checkIns).toHaveLength(2);
+    expect(checkIns).toEqual([
+      expect.objectContaining({ gym_id: "gym-21" }),
+      expect.objectContaining({ gym_id: "gym-22" }),
+    ]);
+  });
+});
